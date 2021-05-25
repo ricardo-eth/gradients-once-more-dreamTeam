94328b088ec8490ba5299d8578801f88eaeda0b0
ricardo-eth <ricardo.soaresdacosta@gmail.com>
2021-05-25 20:06:45 +0200

    Add btn card

--- src/components/Gradient.js
+++ src/components/Gradient.js
@@ -1,9 +1,11 @@
-import GradientTitle from "./GradientTitle"
-import GradientPill from "./GradientPill"
-import GradientCode from "./GradientCode"
-import GradientTags from "./GradientTags"
+import { Link } from "react-router-dom";
 
-const Gradient = ({ colorStart, colorEnd, name, tags }) => {
+import GradientTitle from "./GradientTitle";
+import GradientPill from "./GradientPill";
+import GradientCode from "./GradientCode";
+import GradientTags from "./GradientTags";
+
+const Gradient = ({ colorStart, colorEnd, name, tags, id }) => {
   return (
     <li className="col-lg-3 col-md-4 col-sm-6">
       <div className="card p-3 mb-4 shadow">
@@ -11,9 +13,12 @@ const Gradient = ({ colorStart, colorEnd, name, tags }) => {
         <GradientTitle>{name}</GradientTitle>
         <GradientCode colorStart={colorStart} colorEnd={colorEnd} />
         <GradientTags tags={tags} />
+        <Link className="btn btn-outline-dark w-100" to={`/gradient/${id}`}>
+          Plein écran
+        </Link>
       </div>
     </li>
-  )
-}
+  );
+};
 
-export default Gradient
+export default Gradient;
