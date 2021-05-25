5a6e2d70de3c617ffd3e423d268211a1554a8e1a
johnfr14 <jonathan.alyra@gmail.com>
2021-05-25 15:07:39 +0200

    Route doone

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
 import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom"
-import Gradients from "./components/Gradients"
-import GradientsHeader from "./components/GradientsHeader"
-import Footer from "./components/Footer"
-import { NotFoundPage } from "./components/NotFoundPage"
+import { NotFoundPage } from "./Pages/NotFoundPage"
+import { Home } from "./Pages/Home"
+import { FullPage } from "./Pages/FullPage"
 
 function App() {
   return (
     <Router>
-      <div className="App min-vh-100 d-flex flex-column">
-        <GradientsHeader>
-          <h1 className="display-1">Alyra Gradients</h1>
-          <p className="tagline">Ultime collection de plus beaux dégradés</p>
-        </GradientsHeader>
-        <main className="container">
-          <h1 className="text-center my-4">Alyra Gradients</h1>
-          <Gradients />
-        </main>
-        <Footer />
-      </div>
-
-
       <Switch>
             <Route exact path="/">
               <Home />
@@ -27,7 +13,7 @@ function App() {
             <Route exact path="/gradient/:id">
               <FullPage />
             </Route>
-            <Route path="/404" component={NotFoundPage} />
+            <Route path="*" component={NotFoundPage} />
             <Redirect to="/404" />
           </Switch>
     </Router>
