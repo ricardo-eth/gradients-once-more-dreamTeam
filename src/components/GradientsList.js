f6d56c44c8f31bb36b5c07ee586c425eb102edb3
ricardo-eth <ricardo.soaresdacosta@gmail.com>
2021-05-26 16:40:22 +0200

    Fix key

--- src/components/GradientsList.js
+++ src/components/GradientsList.js
@@ -1,36 +1,33 @@
-import { useFilter } from "../context/FilterContext"
-import  Gradient  from "./Gradient"
+import { useFilter } from "../context/FilterContext";
+import Gradient from "./Gradient";
 
 const GradientsList = () => {
-  
-  const { gradients, loading } = useFilter()
-  console.log(gradients)
-  console.log("gradient")
+  const { gradients, loading } = useFilter();
+  console.log(gradients);
+  console.log("gradient");
 
-  return (
-    loading ? (
-        <p>loading...</p>
-      ) : (
+  return loading ? (
+    <p>loading...</p>
+  ) : (
     <ul className="row list-unstyled">
       {gradients.map((el) => {
-        const { name, start, end, tags, id = [] } = el
-        console.log(start)
-        console.log(end)
-        console.log("colors")
+        const { name, start, end, tags, id = [] } = el;
+        console.log(start);
+        console.log(end);
+        console.log("colors");
         return (
           <Gradient
-            key={name}
+            key={id}
             colorStart={start}
             colorEnd={end}
             name={name}
             tags={tags}
             id={id}
           />
-        )
+        );
       })}
     </ul>
-  )
+  );
+};
 
-)}
-
-export default GradientsList
+export default GradientsList;
