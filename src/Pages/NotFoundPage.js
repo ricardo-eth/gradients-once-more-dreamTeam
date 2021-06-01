9fcf3b8d8078bf8b2e3f7d85316b799a1e730f33
ricardo-eth <ricardo.soaresdacosta@gmail.com>
2021-06-01 10:10:17 +0200

    Add 404 PAGE

--- src/Pages/NotFoundPage.js
+++ src/Pages/NotFoundPage.js
@@ -1,5 +1,26 @@
+import Footer from "../components/Footer";
+import { Link } from "react-router-dom";
+
 export const NotFoundPage = () => {
+  const style = {
+    backgroundImage: `linear-gradient(217deg, rgba(255,0,0,.8), rgba(255,0,0,0) 70.71%),
+            linear-gradient(127deg, rgba(0,255,0,.8), rgba(0,255,0,0) 70.71%),
+            linear-gradient(336deg, rgba(0,0,255,.8), rgba(0,0,255,0) 70.71%)`,
+  };
   return (
-    <>Error 404: page not found loooooool</>
-  )
-}
+    <>
+      <div className="min-vh-100 d-flex flex-column">
+        <div className="flex-fill d-flex" style={style}>
+          <div className="m-auto text-center">
+            <h1 className="text-white display-1">Error 404</h1>
+            <h3 className="text-white">page not found</h3>
+            <Link className="btn btn-dark text-white me-2" to={`/`}>
+              Retour
+            </Link>
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </>
+  );
+};
